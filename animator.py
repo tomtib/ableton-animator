3131ff921b22b9ccf988922a119a5c3e486697c4
Alastair <tibbsalastair@gmail.com>
2021-12-04 15:47:46 +0000

    first commit

---
+++ animator.py
@@ -0,0 +1,194 @@
+import humanizer as hum
+import lfo_generator as lfo_gen
+import random_scene_generator as rsg
+
+import time
+import mido
+import numpy as np
+from scipy.stats import truncnorm
+import multiprocessing
+import threading
+from multiprocessing import Queue
+from multiprocessing.managers import SyncManager
+import re
+import math
+import glob
+
+BPM = 140
+BEATS_PER_BAR = 4
+BEAT_TIME = 60/BPM
+BAR_TIME = BEAT_TIME * BEATS_PER_BAR
+SIXTEENTH_NOTE_TIME = BEAT_TIME / 4
+MIDI_INPUT_PORT = 'humanizer 3'
+MIDI_OUTPUT_PORT = 'loopMIDI Port 3'
+SECTION_CONTROL_LIST = [1, 4, 7, 10, 13, 16, 19, 22]
+RSG_CHANNEL_NO = 1
+LFO_CHANNEL_NO = 2
+MAX_MAIN_WORKERS = 4
+MAX_OUTPUT_WORKERS = 4
+#Input queue must be global for callback
+in_queue = Queue()
+
+def load_sync_file(file_dir, extension):
+    file = glob.glob(file_dir + '/' + '*' + extension)
+    if len(file) != 1:
+        raise ValueError('should be only one txt file in the current directory')
+    file_obj = open(file[0], 'r')
+    for line in file_obj:
+        sync_file = eval(line)
+    if extension == '.rsg':
+        return rsg_sync_file(sync_file)
+    else:
+        return alternative_sync(extension, sync_file)
+        
+def rsg_sync_file(sync_file):
+    section_control_list = []
+    for SECTION_NUMBER in range(len(sync_file)) :
+        section_control_list.append(SECTION_CONTROL_LIST[SECTION_NUMBER])
+    return sync_file, section_control_list
+
+def alternative_sync(extension, sync_file):
+    obj_list = []
+    if extension == '.lfo':
+        for obj in sync_file:
+            obj_list.append(lfo_gen.Lfo(obj[0], obj[1], obj[2]))
+    else:
+        for obj in sync_file:
+            obj_list.append(hum.Cpu(obj[0], obj[1], obj[2]))
+    return obj_list
+
+
+class Worker:
+
+    def __init__(self):
+        pass
+    
+    def send_midi_message(self, message, outport):
+        outport.send(message)   
+        return 
+
+    def add_to_send_queue(self, message, out_queue):
+        out_queue.put(message, block=False)
+        return
+        
+    def read_midi_message(self, msg, metronome_1, human_1, lfo_list, cpu_list, T0, timing_array, metronome_error, out_queue, metronome_timestamp):
+        #Sort message and call relevant functions
+        t1 = time.time()
+        msg_type = msg.type
+        channel = msg.channel
+        msg_str = str(msg)
+        if channel == 1 :
+            if msg_type == 'note_on' and msg.velocity > 10:
+                self.add_to_send_queue(msg, out_queue)
+                human_1.record_timing(timing_array, T0)
+                print('added timing')
+                return
+            if msg_type == 'note_off' :
+                self.add_to_send_queue(msg, out_queue)
+                return
+        if msg_type == 'note_on' :
+            if channel == 0 :
+                metronome_error.value = metronome_1.beat_error(T0)
+                metronome_timestamp.value = time.time()
+                for lfo in lfo_list:
+                    msg = lfo.get_control_value(T0)
+                    self.add_to_send_queue(msg, out_queue)
+                return
+            if channel == 16 :
+                controller = int(re.search(r'note=(.*?) velocity', msg_str).group(1))
+                if controller in section_control_list :
+                    control_message = {'section':section_control_list.index(controller)}
+                    control_message_dict.update(control_message)
+                    return control_message_dict
+            else:
+                timing = cpu_list[channel-1].allocate_timing(timing_array, T0)
+                cpu_list[channel-1].time_message(timing, t1, metronome_error, metronome_timestamp)
+                self.add_to_send_queue(msg, out_queue)
+                return
+
+
+    def multiprocess_init(self, lfo_list, cpu_list, T0, timing_array, metronome_error, metronome_timestamp, out_queue, outport):
+        #Initialise multiprocessing for main workers and message publishers
+        for _ in range(MAX_MAIN_WORKERS):
+            p = multiprocessing.Process(target=self.worker_main, args=(lfo_list, cpu_list, T0, timing_array, metronome_error, out_queue, metronome_timestamp))
+            p.start()
+        for _ in range(MAX_OUTPUT_WORKERS):
+            q = threading.Thread(target=self.output_worker, args=(out_queue, outport))
+            q.start()
+        return 
+
+    def worker_main(self, lfo_list, cpu_list, T0, timing_array, metronome_error, out_queue, metronome_timestamp):
+        #Main worker initialisation and loop
+        metronome_1 = hum.Metronome(0)
+        human_1 = hum.Human(1)
+        while 1:
+            msg = in_queue.get()
+            self.read_midi_message(msg, metronome_1, human_1, lfo_list, cpu_list, T0, timing_array, metronome_error, out_queue, metronome_timestamp)
+    
+    def output_worker(self, out_queue, outport):
+        #Publish output messages from main workers
+        while 1:
+                msg = out_queue.get()
+                self.send_midi_message(msg, outport)
+                
+    def input_worker(self, msg):
+        #Called when message arrives in midi input port
+        in_queue.put(msg, block=False)
+        return
+    
+    
+
+if __name__=='__main__':
+    print("\n--Program Setup--\n")
+    inport, outport = rsg.open_midi_ports(MIDI_INPUT_PORT, MIDI_OUTPUT_PORT)
+    if input('Load sync files? y/n : ') == 'y' :
+        file_dir = input('Please enter file directory : ')
+        file_extension = '.rsg'
+        ALL_SECTIONS_ARRAY, section_control_list = load_sync_file(file_dir, file_extension)
+        file_extension = '.lfo'
+        lfo_list = load_sync_file(file_dir, file_extension)
+        file_extension = '.hum'
+        cpu_list = load_sync_file(file_dir, file_extension)    
+    else :
+        print('proceeding with manual sync...')
+        input("\nPress enter to start scene_gen sync.")
+        ALL_SECTIONS_ARRAY, section_control_list = rsg.sync_song(SECTION_CONTROL_LIST)
+        print('Sync finished.')
+        rsg.write_sync_file(ALL_SECTIONS_ARRAY)
+        input("\nPress enter to start lfo sync.")
+        lfo_list = load_sync_file(file_dir, file_extension)
+        lfo.lfo_sync(LFO_CHANNEL_NUMBER, lfo_list)
+        cpu_list = load_sync_file(file_dir, file_extension)
+    print('Initialising class instances...')
+    metronome_1 = hum.Metronome(0)
+    worker = Worker()
+    out_queue = Queue()
+    print('Setting up process manager...')
+    manager = SyncManager()
+    manager.start()
+    scalar_timing_array = np.ndarray.tolist(np.zeros((16,5)))
+    timing_array = manager.list(scalar_timing_array)
+    metronome_error = manager.Value('f', 0)
+    T0 = manager.Value('f', time.time())
+    metronome_timestamp = manager.Value('f', 0)
+    control_message_dict = {'section':0}
+    control_dict = manager.dict(control_message_dict)
+    print('Initialising multiprocessing...')
+    worker.multiprocess_init(lfo_list, cpu_list, T0, timing_array, metronome_error, metronome_timestamp, out_queue, outport)
+    while 1:
+        metronome_1.count_in()
+        inport.callback = conductor_1.input_worker
+        ALL_TRACKS_ARRAY = ALL_SECTIONS_ARRAY[section]
+        print('program starting...')
+        while 1:
+            if section != control_dict.get('section'):
+                section = control_dict.get('section')
+                section_string = section + 1
+                print(f'Now playing section {section_string}')
+                ALL_TRACKS_ARRAY = ALL_SECTIONS_ARRAY[section]
+                run_section(BAR_LENGTH, ALL_TRACKS_ARRAY)
+            else:
+                run_section(BAR_LENGTH, ALL_TRACKS_ARRAY)
+            if msvcrt.kbhit():
+                if ord(msvcrt.getch()) == 27 :
+                    break
---
+++ lfo_generator.py
@@ -0,0 +1,150 @@
+import mido
+from mido import Message
+import time
+#from collections import deque
+import numpy as np
+import re
+
+LFO_CHANNEL_NO = 15
+BPM = 140
+BEATS_PER_BAR = 4
+BEAT_TIME = 60/BPM
+SIXTEENTH_NOTE_TIME = BEAT_TIME / 4
+BAR_TIME = BEAT_TIME * BEATS_PER_BAR
+MIDI_INPUT_PORT = 'humanizer 2'
+MIDI_OUTPUT_PORT = 'loopMIDI Port 2'
+
+#Program initialise functions
+
+def open_midi_ports(MIDI_INPUT_PORT, MIDI_OUTPUT_PORT):
+    print('\n')
+    print('INPUT PORTS: ')
+    print (mido.get_input_names())
+    print('\n')
+    print('OUTPUT PORTS: ')
+    print (mido.get_output_names())
+    inport = mido.open_input(MIDI_INPUT_PORT)
+    outport = mido.open_output(MIDI_OUTPUT_PORT)
+    print(f'Connected input to {MIDI_INPUT_PORT}')
+    print(f'Connected output to {MIDI_OUTPUT_PORT}')
+    return inport, outport
+
+def send_midi_message(value):
+    msg = mido.Message('control_change', channel=CHANNEL_NUMBER, control=1, value=value)
+    outport.send(msg)
+    return
+
+def count_in():
+    #Count in to sync program with ableton
+    counter = 0
+    bars = int(input('Enter number of count in bars: '))
+    total_beats = bars * 16 + 1
+    for msg in inport:
+        msg_str = str(msg)
+        channel = int(re.search(r'channel=(.*?) ', msg_str).group(1))
+        if channel == 0 and bool(re.search(r'note_on', msg_str)):
+            counter = counter + 1
+            if counter % 4 == 0:
+                print((((total_beats - counter)-1)/4)+1)
+        if counter >= total_beats:
+            return
+
+def lfo_sync(CHANNEL_NUMBER, lfo_list):
+    message = ''
+    print('Press esc for next lfo')
+    for lfo in lfo_list:
+        control_number = 0
+        print (f'...CC {control_number}')
+        msg = mido.Message('control_change', channel=CHANNEL_NUMBER, control=control_number)
+        while 1:
+            time.sleep(2)
+            outport.send(msg)
+            if msvcrt.kbhit():
+                if ord(msvcrt.getch()) == 27 :
+                    break
+        control_number += 1
+    return
+    
+    
+    
+
+class Lfo:
+
+    def __init__(self, control_number, frequency, offset):
+        self.control_number = control_number
+        self.frequency = frequency
+        self.offset = offset / 360 * 2 * np.pi
+
+    def check_time(self, T0):
+        current_time = time.time() - T0
+        return current_time
+
+    def get_beat_number(self, T0):
+        #Return current 16th note beat number
+        beat_number = -1
+        bar_time_passed = self.check_time(T0) - BAR_TIME * self.get_bar_number(T0)
+        while bar_time_passed > 0:
+            bar_time_passed = bar_time_passed - SIXTEENTH_NOTE_TIME
+            beat_number = beat_number + 1
+        return beat_number
+
+    def get_bar_number(self, T0):
+        #Return current bar number
+        bar_number = -1
+        track_time_passed = self.check_time(T0)
+        while track_time_passed > 0:
+            track_time_passed = track_time_passed - BAR_TIME
+            bar_number = bar_number + 1
+        return bar_number
+
+    def get_beats_passed_time(self, T0):
+        #Return length of bar passed so far
+        beats_passed_time = (self.get_beat_number(T0))*SIXTEENTH_NOTE_TIME
+        return beats_passed_time
+
+    def get_bars_passed_time(self, T0):
+        #Return bar time passed so far
+        bars_passed_time = self.get_bar_number(T0)*BAR_TIME
+        return bars_passed_time
+
+    def get_control_value(self, T0):
+        radians = ((self.get_beats_passed_time(T0) / BAR_TIME) * 2 * np.pi + self.offset) * self.frequency 
+        amplitude = np.sin(radians)
+        control_value = int(round((amplitude + 1) / 2 * 127))
+        #print(control_value)
+        #print(radians / (2 * np.pi) * 360)
+        msg = mido.Message('control_change', channel=LFO_CHANNEL_NO, control=self.control_number, value=127)
+        return msg
+
+    def map_midi(self):
+
+        outport.send(msg)
+        return
+    
+
+if __name__ == "__main__":
+    inport, outport = open_midi_ports(MIDI_INPUT_PORT, MIDI_OUTPUT_PORT)
+    lfo_1 = Lfo(1, 1.5, 180)
+    lfo_2 = Lfo(2, 2, 0)
+    lfo_3 = Lfo(3, 0.75, 180)
+    lfo_4 = Lfo(4, 0.4, 0)
+    lfo_5 = Lfo(5, 3, 180)
+    input('Press enter to start sync...')
+    #lfo_1.map_midi()
+    #lfo_2.map_midi()
+    #lfo_3.map_midi()
+    #lfo_4.map_midi()
+    lfo_5.map_midi()
+    count_in()
+    T0 = time.time()
+    while 1:
+        control_value = lfo_1.get_control_value(T0)
+        send_midi_message(control_value)
+        control_value = lfo_2.get_control_value(T0)
+        send_midi_message(control_value)
+        control_value = lfo_3.get_control_value(T0)
+        send_midi_message(control_value)
+        control_value = lfo_4.get_control_value(T0)
+        send_midi_message(control_value)
+        time.sleep(SIXTEENTH_NOTE_TIME)
+    
